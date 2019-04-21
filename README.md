f03182d19b1acb1d0d82c7848349e9673de50dbc
Eran Hammer <eran@hammer.io>
2019-04-21 16:30:10 -0700

    Change namespace. Closes #3

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
-# hapi-no-var
+# no-var
 
+ESLint rule to enforce the usage of var declarations only in try-catch scope.
--- test/index.js
+++ test/index.js
@@ -1,15 +1,14 @@
 'use strict';
 
-// Load modules
-
-const Code = require('code');
+const Code = require('@hapi/code');
 const ESLint = require('eslint');
-const Lab = require('lab');
-const HapiNoVar = require('../lib');
+const Lab = require('@hapi/lab');
+const NoVar = require('..');
 
 
 const { describe, it } = exports.lab = Lab.script();
 const RuleTester = ESLint.RuleTester;
+
 
 Code.settings.truncateMessages = false;
 
@@ -28,7 +27,7 @@ describe('ESLint Rule', () => {
         ];
 
         const ruleTester = new RuleTester({ parserOptions: { ecmaVersion: 6 } });
-        ruleTester.run(HapiNoVar.esLintRuleName, HapiNoVar, {
+        ruleTester.run(NoVar.esLintRuleName, NoVar, {
             valid: [],
             invalid: sample.map((code) => {
 
@@ -51,7 +50,7 @@ describe('ESLint Rule', () => {
         ];
 
         const ruleTester = new RuleTester({ parserOptions: { ecmaVersion: 6 } });
-        ruleTester.run(HapiNoVar.esLintRuleName, HapiNoVar, {
+        ruleTester.run(NoVar.esLintRuleName, NoVar, {
             valid: sample.map((code) => {
 
                 return { code };
